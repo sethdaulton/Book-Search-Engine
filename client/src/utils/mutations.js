44a7f0833d704c47b3a3cd94e3ca81f9be1b67a1
Seth Daulton <seth.daulton@gmail.com>
2023-02-22 14:55:21 -0600

    Made updates to some front end files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,11 +3,28 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation loginUser
+  mutation login($email: String!, $password: String!) {
+    login(email:$email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+
 `;
 
 export const ADD_USER = gql`
-  mutation addUser
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 export const SAVE_BOOK = gql`
